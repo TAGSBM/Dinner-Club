9e4082bf11769abe57053aab6cdef64309c0ebd5
SythilBlade <stevewright2009@gmail.com>
2015-06-01 12:59:35 +1000

    4
    54

---
+++ dinner_club_members/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Dinner Club Members",
+    'version': "1.0",
+    'author': "tagsbm",
+    'category': "Tools",
+    'depends': ['crm','membership','mass_mailing'],
+    'data': ['dinner_club.xml'],
+    'demo': [],
+    'installable': True,
+}
